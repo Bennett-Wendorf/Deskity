538bc7b804bba7d411e73817890f2d12f49a283e
Bennett-Wendorf <bennettwendorf@yahoo.com>
2019-10-23 20:05:19 -0500

    continuted learning Kivy

--- kivy_venv_raspi_desk_stats/App/main.py
+++ kivy_venv_raspi_desk_stats/App/main.py
@@ -3,6 +3,8 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.checkbox import CheckBox
 
 class ToDoWidget(Widget):
     def on_touch_down(self, touch):
@@ -12,9 +14,10 @@ class RaspiDeskStatsApp(App):
     def build(self):
         mainBoxLayout = BoxLayout(orientation='horizontal')
         leftBoxLayout = BoxLayout(orientation='vertical')
-        appLauncher = Widget()
+        appLauncher = BoxLayout(orientation='horizontal')
+        appLauncher.add_widget(CheckBox(pos=(50,150)))
         leftBoxLayout.add_widget(appLauncher)
-        leftBoxLayout.add_widget(Button(text='App location'))
+        leftBoxLayout.add_widget(Button(text="App Location Placeholder"))
         toDo = Label(text="To Do List Placeholder")
         mainBoxLayout.add_widget(leftBoxLayout)
         mainBoxLayout.add_widget(toDo)
