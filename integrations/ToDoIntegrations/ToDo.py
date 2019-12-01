0a83710e6ec200003eaaa03a6f19f40256d919af
Bennett-Wendorf <bennettwendorf@yahoo.com>
2019-12-01 11:09:17 -0600

    Working on handling GET request form msal.

--- integrations/ToDoIntegrations/ToDo.py
+++ integrations/ToDoIntegrations/ToDo.py
@@ -3,17 +3,31 @@ import yaml
 from requests_oauthlib import OAuth2Session
 import os
 import webbrowser
+import http.server
+import threading
+
+#class RequestHandler(BaseHTTPRequestHandler):
+ #   def do_GET(self):
+
+
 
 def aquireAuthCode(settings):
     authorize_url = '{0}{1}'.format(settings['authority'], settings['authorize_endpoint'])
     print("Authorize URL:", authorize_url)
     aadAuth = OAuth2Session(settings['app_id'], scope=settings['scopes'])#, redirect_uri=settings['redirect_uri'])
     sign_in_url, state = aadAuth.authorization_url(authorize_url, prompt='login')
+    webServerThread = threading.Thread(target=RunLocalhostServer)
+    webServerThread.setDaemon(True)
+    webServerThread.start()
     print("Sign in URL:", sign_in_url)
     webbrowser.open(sign_in_url, new=1, autoraise=True)
     authorization_response = input('Enter the auth code: ')
     return authorization_response
 
+def RunLocalhostServer(server_class=http.server.HTTPServer, handler_class=http.server.BaseHTTPRequestHandler):
+    server_address = ('127.0.0.1', 80)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
 
 if __name__ == '__main__':
     # This is necessary because Azure does not guarantee
