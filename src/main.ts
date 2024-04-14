834c0bb496234fc4031471784880650ff4dc31a2
Bennett-Wendorf <bennettwendorf@gmail.com>
2024-04-13 22:02:36 -0500

    Create Tauri App

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./styles.css";
+import App from "./App.svelte";
+
+const targetElement = document.getElementById("app");
+const app = new App({
+  target: targetElement ? targetElement : document.body,
+});
+
+export default app;
