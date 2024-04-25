c1d02982a6c0f413965fb218ccd7c9063046c789
Bennett-Wendorf <bennettwendorf@gmail.com>
2024-04-24 22:12:27 -0500

    Finished formatting logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,33 @@
 
 mod settings;
 
+use log::warn;
+use tauri_plugin_log::{Target, TargetKind};
+use chrono::Local;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
+    warn!("This is a warning message from Rust");
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().targets([
+                Target::new(TargetKind::Stdout),
+                Target::new(TargetKind::Webview),
+            ])
+            .format(|callback, message, record| {
+                callback.finish(format_args!(
+                    "{}[{}:{}][{}] {}", 
+                    Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                    record.file().unwrap_or("unknown"),
+                    record.line().unwrap_or(0),
+                    tauri_plugin_log::fern::colors::ColoredLevelConfig::default().color(record.level()), 
+                    message))
+            })
+            .build())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src/components/Greet.svelte
+++ src/components/Greet.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core"
+  import { error } from "@tauri-apps/plugin-log"
 
   let name = "";
   let greetMsg = ""
@@ -7,6 +8,8 @@
   async function greet(){
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     greetMsg = await invoke("greet", { name })
+
+    error("Testing logging from Tauri plugin...");
   }
 </script>
 
